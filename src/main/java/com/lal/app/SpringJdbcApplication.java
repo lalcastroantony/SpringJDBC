38550ff7406df5504dba7b84f02c17b5c38b0025
Lal Castro <lal.castro@icloud.com>
2025-04-25 21:57:52 +0530

    Refactors student retrieval using lambda expression
    Simplifies the student retrieval logic by replacing the anonymous inner class RowMapper with a lambda expression in the `findAll` method of the `StudentRepo` class. This improves code readability and maintainability.

--- src/main/java/com/lal/app/SpringJdbcApplication.java
+++ src/main/java/com/lal/app/SpringJdbcApplication.java
@@ -14,16 +14,16 @@ public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
-		
+
 		Student s = context.getBean(Student.class);
 		s.setRollNo(101);
 		s.setName("Lal");
 		s.setMarks(104);
-		
+
 		StudentService service = context.getBean(StudentService.class);
-		
+
 		service.addStudent(s);
-		
+
 		List<Student> students = service.getStudents();
 		System.out.println(students);
 	}
--- src/main/java/com/lal/app/repo/StudentRepo.java
+++ src/main/java/com/lal/app/repo/StudentRepo.java
@@ -1,12 +1,9 @@
 package com.lal.app.repo;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.lal.app.model.Student;
@@ -36,20 +33,14 @@ public class StudentRepo {
 	}
 
 	public List<Student> findAll() {
-
 		String sql = "select * from student";
-		RowMapper<Student> mapper = new RowMapper<Student>() {
-
-			@Override
-			public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Student s = new Student();
-				s.setRollNo(rs.getInt("rollno"));
-				s.setName(rs.getString("name"));
-				s.setMarks(rs.getInt("marks"));
-				return s;
-			}
-		};
-		return jdbc.query(sql, mapper);
+		return jdbc.query(sql, (rs, rowNum) -> {
+			Student s = new Student();
+			s.setRollNo(rs.getInt("rollno"));
+			s.setName(rs.getString("name"));
+			s.setMarks(rs.getInt("marks"));
+			return s;
+		});
 	}
 
 }
