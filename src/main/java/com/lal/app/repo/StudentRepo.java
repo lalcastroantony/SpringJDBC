d01304aa40335dd4e715aef0cc762d82c7c78761
Lal Castro <lal.castro@icloud.com>
2025-04-25 21:53:41 +0530

    Implements retrieval of all students.
    Implements the functionality to retrieve all students from the database.
    Uses JdbcTemplate to query the database and a RowMapper to map each row to a Student object.

--- src/main/java/com/lal/app/repo/StudentRepo.java
+++ src/main/java/com/lal/app/repo/StudentRepo.java
@@ -1,46 +1,55 @@
 package com.lal.app.repo;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.lal.app.model.Student;
 
 @Repository
 public class StudentRepo {
-	
+
 	private JdbcTemplate jdbc;
 
 	public void save(Student s) {
-		
-		String sql = "insert into student (rollno, name, marks) values (?,?,?)";
-		
-		int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
-		
-		System.out.println(rows + " effected");
-		
-	}
 
+		String sql = "insert into student (rollno, name, marks) values (?,?,?)";
+
+		int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
+
+		System.out.println(rows + " effected");
+
+	}
 
 	public JdbcTemplate getJdbc() {
 		return jdbc;
 	}
-
 
 	@Autowired
 	public void setJdbc(JdbcTemplate jdbc) {
 		this.jdbc = jdbc;
 	}
 
-
 	public List<Student> findAll() {
-		
-		List<Student> students = new ArrayList<>();
-		
-		return students;
+
+		String sql = "select * from student";
+		RowMapper<Student> mapper = new RowMapper<Student>() {
+
+			@Override
+			public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Student s = new Student();
+				s.setRollNo(rs.getInt("rollno"));
+				s.setName(rs.getString("name"));
+				s.setMarks(rs.getInt("marks"));
+				return s;
+			}
+		};
+		return jdbc.query(sql, mapper);
 	}
-	
+
 }
